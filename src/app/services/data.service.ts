e430cf87418da1575763d74e5b31a28e07b24c0f
Gauravdarkslayer <www.gaurav10bhojwani@gmail.com>
2023-02-08 00:24:05 +0530

    can now be able to create collections and insert data in it

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -5,14 +5,16 @@ import * as Realm from 'realm-web'
 @Injectable()
 export class DataService {
     realmApp!: Realm.App;
-
+    mongoClient: any;
     constructor(private snackBar:MatSnackBar) {
-        // this.initApp();
-        // this.loginEmailPassword('gaurav@gmail.com','1234412344')
+        this.initApp();
+        this.loginEmailPassword('gaurav@gmail.com','1234412344')
     }
 
     async initApp() {
-        this.realmApp = new Realm.App({ id: "application-0-igsoe" });
+        this.realmApp = new Realm.App({ id: "application-0-kmsrp" });
+        this.mongoClient = this.realmApp.currentUser!.mongoClient('mongodb-atlas');
+        this.createOrConnectWithCollection()
     }
 
     async loginEmailPassword(email: string, password: string) {
@@ -23,10 +25,16 @@ export class DataService {
         // `App.currentUser` updates to match the logged in user
         console.assert(user.id === this.realmApp.currentUser!.id);
         console.log(user);
-        let snackBarRef = this.snackBar.open('Welcome ' + user.profile.email,'',{
+        this.snackBar.open('Welcome ' + user.profile.email,'',{
             duration: 3000
           });
 
         return user;
+    }
+
+    async createOrConnectWithCollection() {
+        const collection = this.mongoClient.db('investment-tracker').collection('category');
+        console.log(await collection.insertOne({name:"MyFirstCategory"}))
+        return collection;
     }
 }
