2122af60dd2d4b55d4756b07120e83055e91401f
Gauravdarkslayer <www.gaurav10bhojwani@gmail.com>
2023-02-19 23:12:05 +0530

    working on investment calculations

--- src/app/services/fd-calculations.service.ts
+++ src/app/services/fd-calculations.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { Investment } from "../interfaces/investment.interface";
 
 @Injectable()
 export class FdCalculations {
@@ -8,7 +9,26 @@ export class FdCalculations {
 
     }
 
-    allFdInCategoryCalculations() {
+    allFdInCategoryCalculations(investmentsData: Array<Investment>) {
+        // average return
+        let averageReturn = 0;
+        let investedValue = 0;
+        let interestEarned = 0;
+        for (const investment of investmentsData) {
+            averageReturn += investment.rateOfInterest;
+            investedValue += investment.investmentAmount;
+
+            // interest earned
+            investment.investmentAmount
+            investment.rateOfInterest
+            investment.startDate
+            const today = Date.now();   
+
+
+
+        }
+        averageReturn = averageReturn / investmentsData.length;
+
 
     }
 
